75e36201bbdc34e4859069a7be52724b09161cf0
dj0mlinho <mladenmilinkovic88@gmail.com>
2019-05-19 16:38:41 +0200

    Initial commit

---
+++ workorders.service.js
@@ -0,0 +1,48 @@
+export class WorkordersService {
+  workorders = [
+    {
+      id: 1,
+      buildingNmb: 1,
+      apartmentNmb: 1,
+      selectDate: new Date(),
+      completeDate: new Date(),
+      sendDate: new Date(),
+      user: 'Anastazija Veselinovic',
+      status: 'sent'
+    },
+    {
+      id: 2,
+      buildingNmb: 2,
+      apartmentNmb: 1,
+      selectDate: new Date(),
+      completeDate: new Date(),
+      sendDate: new Date(),
+      user: 'Stefan Djordjevic',
+      status: 'pending'
+    },
+    {
+      id: 3,
+      buildingNmb: 1,
+      apartmentNmb: 2,
+      selectDate: new Date(),
+      completeDate: new Date(),
+      sendDate: new Date(),
+      user: 'Anastazija Veselinovic',
+      status: 'sent'
+    },
+    {
+      id: 4,
+      buildingNmb: 8,
+      apartmentNmb: 1,
+      selectDate: new Date(),
+      completeDate: new Date(),
+      sendDate: new Date(),
+      user: 'Stefan Djordjevic',
+      status: 'sent'
+    }
+  ];
+
+  getAllWorkorders(): Workorder[] {
+    return this.workorders;
+  }
+}
